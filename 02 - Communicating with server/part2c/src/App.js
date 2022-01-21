afcc20acbbfc1b33b72419bb58352066922cb2c5
bvu12 <brian.vu12@gmail.com>
2022-01-21 15:56:58 -0800

    working through part2d, altering data in server

--- 02 - Communicating with server/part2c/src/App.js
+++ 02 - Communicating with server/part2c/src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import Notes from './components/Notes'
+import Note from './components/Note'
+import noteService from './services/notes'
 
 
 const App = () => {
@@ -8,18 +8,15 @@ const App = () => {
   const [newNote, setNewNote] = useState ('')
   const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }
 
   // [] means that the effect is only run along with the first render of the component.
-  useEffect(hook, [])
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
@@ -27,11 +24,16 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random < 0.5,
-      id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    // POST: create new 
+    noteService
+      .create(noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+      
   }
 
   const handleNoteChange = (event) => {
@@ -44,6 +46,23 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
+
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note,
+      important: !note.important}
+
+    // PUT = insert, replace if already exists
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ?
+          note:
+          response.data))
+      })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -53,7 +72,13 @@ const App = () => {
           show {showAll ? 'important' : 'all'}
         </button>
       </div>
-      <Notes notes={notesToShow}/>
+      <ul>
+        {notesToShow.map((note) =>
+          <Note 
+          key={note.id}
+          note={note}
+          toggleImportance={() => toggleImportanceOf(note.id)}/>)}
+      </ul>
       <form onSubmit={addNote}>
         <input 
           value = {newNote}
