ab33b46ea8df66339d36180006bc714600aee747
bvu12 <brian.vu12@gmail.com>
2022-01-31 13:38:02 -0800

    part 3 server

---
+++ 03 - Programming a server/index.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const Note = require('./models/note')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+// Handles HTTP GET requests made to /root
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+// Handles HTTP GET requests made to /notes
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+    })
+})
+
+// Delete request
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    // Status 204: no content
+    response.status(204).end()
+})
+
+// Post request
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (body.content === undefined) {
+        return response.status(400).json({error: 'content missing'})
+    }
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+    })
+
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
